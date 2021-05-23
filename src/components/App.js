a010ef65c28dc5d4471a7602036628bd46519791
pubehunter <manishchandra58@gmail.com>
2021-05-23 17:46:19 +0530

    Redux-store-subscribed-dispatched

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,41 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
+class App extends React.Component{
+  
+  componentDidMount(){
+    const {store} = this.props;
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='main'>
-        <div className='tabs'>
-          <div className='tab'>Movies</div>
-          <div className='tab'>Favourites</div>
-        </div>
-        <div className='list'>
-          {data.map((movie)=>{
-            return <MovieCard movie={movie} />
-          })}
+    store.subscribe(()=>{
+      console.log("updated");
+      this.forceUpdate();
+    })
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies:data
+    });
+  }
+  
+  
+  render(){
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className='main'>
+          <div className='tabs'>
+            <div className='tab'>Movies</div>
+            <div className='tab'>Favourites</div>
+          </div>
+          <div className='list'>
+            {movies.map((movie,index)=>{
+              return <MovieCard movie={movie} key={`movie-${index}`} />
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+
+
 import './index.css';
 import App from './components/App';
+import Movies from './reducers';
 
+const store = createStore(Movies);
+console.log("Store", store);
+console.log("STATE", store.getState());
+
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'Superman'}]
+});
+console.log("STate later", store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
